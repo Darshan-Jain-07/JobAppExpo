b6c5d4df7b6bb4bd89079ae8dc37d70efb74cc96
Darshan <jdarshan535@gmail.com>
2025-03-28 00:40:37 +0530

    Remove Txet

--- src/screens/jobSeeker/CompanyProfile.js
+++ src/screens/jobSeeker/CompanyProfile.js
@@ -149,7 +149,6 @@ const CompanyProfileScreen = () => {
         renderItem={renderItem}
         ListFooterComponent={<View style={styles.footer}></View>}
       />
-      <Text>Hello</Text>
     </View>
   );
 };
